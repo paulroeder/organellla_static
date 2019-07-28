58706956c3dce9eb5f03bcd16c0c597c7946224d
paulroeder <pr@dev.paulroeder.de>
2019-07-28 17:48:04 +0200

    organella static first version

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var granimInstance = new Granim({
+   element: '#canvas-basic',
+   name: 'granim',
+   opacity: [1, 1],
+   states : {
+       "default-state": {
+           gradients: [
+               ['#40322E', '#3F322E'],
+               ['#130808', '#40322E']
+           ]
+       }
+   }
+});
